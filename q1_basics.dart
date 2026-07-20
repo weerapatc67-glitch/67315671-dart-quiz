9d5151365fefc3dc549db905a0f89f7adbb6460d
weerapatc67-glitch <weerapatc67@nu.ac.th>
2026-07-20 18:09:38 +0700

    ซ่อมdart Quiz

--- q1_basics.dart
+++ q1_basics.dart
@@ -34,7 +34,8 @@ void main() {
   double calcTotal(int price, int qty, [double discount = 0]) {
     return (price * qty - discount) * (1 + vatRate);
   }
-
+  print('---');
+  
   print('ยอดสุทธิ (ไม่มีส่วนลด): ${calcTotal(55, 2)} บาท');
   print('ยอดสุทธิ (ส่วนลด10บาท): ${calcTotal(55, 2, 10)} บาท');
 
